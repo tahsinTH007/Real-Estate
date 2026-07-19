0abaae1fe79b3c6b2a82f9f6cde26cfbfd71082d
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-07-19 20:24:17 +0600

    Server: get leases payments

--- server/src/controllers/leaseControllers.ts
+++ server/src/controllers/leaseControllers.ts
@@ -19,3 +19,19 @@ export const getLeases = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+export const getLeasePayments = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const payments = await prisma.payment.findMany({
+      where: { leaseId: Number(id) },
+    });
+    res.json(payments);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving lease payments: ${error.message}` });
+  }
+};
--- server/src/routes/leaseRoutes.ts
+++ server/src/routes/leaseRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { getLeases } from "../controllers/leaseControllers";
+import { getLeasePayments, getLeases } from "../controllers/leaseControllers";
 
 const router = express.Router();
 
 router.get("/", authMiddleware(["manager", "tenant"]), getLeases);
+router.get(
+  "/:id/payments",
+  authMiddleware(["manager", "tenant"]),
+  getLeasePayments,
+);
 
 export default router;
