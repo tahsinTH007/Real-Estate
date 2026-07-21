a395bb516ba0fb92f7eee15451bbadd7ed61d9d0
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-07-21 21:30:42 +0600

    Server: get current tenant

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createTenant, getTenant, updateTenant } from "../controllers/tenantControllers";
+import {
+  createTenant,
+  getCurrentResidences,
+  getTenant,
+  updateTenant,
+} from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
 
 export default router;
